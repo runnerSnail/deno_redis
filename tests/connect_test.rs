3497ed64f0886802415f461d09519bf011a0590f
runnerSnail <gaomengjie_forever@163.com>
2020-03-18 23:44:47 +0800

    change:create project deno_redis

---
+++ tests/connect_test.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn connect_with_options_test() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    fn connect_with_url_test() {
+        assert_eq!(2 + 2, 4);
+    }
+}
